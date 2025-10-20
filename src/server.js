ef814d7a6767edc1acafae9b31a19a028b237275
guruprasad <guruprasadsali378@gmail.com>
2025-10-20 11:17:10 +0530

    Updated server

--- src/server.js
+++ src/server.js
@@ -8,6 +8,9 @@ connectDB();
 
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
